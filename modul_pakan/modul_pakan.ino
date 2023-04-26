8e52dd280091e1c3665800f9c1d987665de0d869
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-04-26 10:54:01 +0700

    rapihin modul pakan

--- modul_pakan/modul_pakan.ino
+++ modul_pakan/modul_pakan.ino
@@ -9,74 +9,74 @@ int tanggal, bulan, tahun, jam, menit, detik;
 #include <Servo.h>
 Servo servoku;
 
-void setup () {
-  Serial.begin(9600);
+void setup() {
+    Serial.begin(9600);
 
-  servoku.attach(3);
+    servoku.attach(3);
 
-  if (! rtc.begin()) {
-    Serial.println("RTC Tidak Ditemukan");
-    Serial.flush();
-    abort();
-  }
+    if (! rtc.begin()) {
+        Serial.println("RTC Tidak Ditemukan");
+        Serial.flush();
+        abort();
+    }
 
-  //Atur Waktu
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  //  rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    //Atur Waktu
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    //  rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 }
 
-void loop () {
-  DateTime now = rtc.now();
-  hari    = dataHari[now.dayOfTheWeek()];
-  tanggal = now.day(), DEC;
-  bulan   = now.month(), DEC;
-  tahun   = now.year(), DEC;
-  jam     = now.hour(), DEC;
-  menit   = now.minute(), DEC;
-  detik   = now.second(), DEC;
+void loop() {
+    DateTime now = rtc.now();
+    hari    = dataHari[now.dayOfTheWeek()];
+    tanggal = now.day(), DEC;
+    bulan   = now.month(), DEC;
+    tahun   = now.year(), DEC;
+    jam     = now.hour(), DEC;
+    menit   = now.minute(), DEC;
+    detik   = now.second(), DEC;
 
-//  Serial.println(String() + hari + ", " + tanggal + "-" + bulan + "-" + tahun);
-//  Serial.println(String() + jam + ":" + menit + ":" + detik);
-//  Serial.println();
+    //  Serial.println(String() + hari + ", " + tanggal + "-" + bulan + "-" + tahun);
+    //  Serial.println(String() + jam + ":" + menit + ":" + detik);
+    //  Serial.println();
 
-//  modulPakan();
+    //  modulPakan();
 
-  if(jam == 13 && menit == 49 && detik == 10) {
-    modulPakan(); 
-  }
+    if(jam == 13 && menit == 49 && detik == 10) {
+        modulPakan(); 
+    }
 
-  if(jam == 13 && menit == 49 && detik == 30) {
-    modulPakan(); 
-  }
+    if(jam == 13 && menit == 49 && detik == 30) {
+        modulPakan(); 
+    }
 
-  if(jam == 13 && menit == 49 && detik == 50) {
-    modulPakan(); 
-  }
+    if(jam == 13 && menit == 49 && detik == 50) {
+        modulPakan(); 
+    }
 
-//  if(jam == 7 && menit == 0 && detik == 0) {
-//    modulPakan(); 
-//  }
-//
-//  if(jam == 12 && menit == 0 && detik == 0) {
-//    modulPakan(); 
-//  }
-//
-//  if(jam == 17 && menit == 0 && detik == 0) {
-//    modulPakan(); 
-//  }
+    //  if(jam == 7 && menit == 0 && detik == 0) {
+    //    modulPakan(); 
+    //  }
+    //
+    //  if(jam == 12 && menit == 0 && detik == 0) {
+    //    modulPakan(); 
+    //  }
+    //
+    //  if(jam == 17 && menit == 0 && detik == 0) {
+    //    modulPakan(); 
+    //  }
 }
 
 void modulPakan() {
-  // proses perputaran pakan secara halus
-  for (int posisi = 0; posisi <= 180; posisi++) {
-    // set posisi servo berdasarkan variabel posisi
-    servoku.write(posisi);
-    delay(10);
-  }
+    // proses perputaran pakan secara halus
+    for (int posisi = 0; posisi <= 180; posisi++) {
+        // set posisi servo berdasarkan variabel posisi
+        servoku.write(posisi);
+        delay(10);
+    }
 
-  // proses perputaran tempat pakan kembali ke posisi awal
-  for (int posisi = 180; posisi >= 0; posisi--) {
-    servoku.write(posisi);
-    delay(10);
-  }
+    // proses perputaran tempat pakan kembali ke posisi awal
+    for (int posisi = 180; posisi >= 0; posisi--) {
+        servoku.write(posisi);
+        delay(10);
+    }
 }
