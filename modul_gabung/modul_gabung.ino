8fb9cc3231ff4fae79258424d1fa02acfbd56456
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-05-06 15:38:02 +0700

    add modul_pakan di modul_gabung

--- modul_gabung/modul_gabung.ino
+++ modul_gabung/modul_gabung.ino
@@ -1,9 +1,76 @@
-void setup() {
-  // put your setup code here, to run once:
+//rtc
+#include "RTClib.h"
+RTC_DS3231 rtc;
+char dataHari[7][12] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
+String hari;
+int tanggal, bulan, tahun, jam, menit, detik;
+//rtc
 
+//servo
+#include <Servo.h>
+Servo servoku;
+//servo
+
+void setup() {
+    Serial.begin(9600);
+
+    // servo
+    servoku.attach(3);
+
+    // rtc ds3231
+    if (! rtc.begin()) {
+        Serial.println("RTC Tidak Ditemukan");
+        Serial.flush();
+        abort();
+    }
+
+    //Atur Waktu
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    //  rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    // rtc ds3231
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // BACA WAKTU
+    DateTime now = rtc.now();
+    hari    = dataHari[now.dayOfTheWeek()];
+    tanggal = now.day(), DEC;
+    bulan   = now.month(), DEC;
+    tahun   = now.year(), DEC;
+    jam     = now.hour(), DEC;
+    menit   = now.minute(), DEC;
+    detik   = now.second(), DEC;
+    // BACA WAKTU
 
+    // KONDISI MODUL PAKAN
+    // kondisi coba2
+    if(jam == 13 && menit == 49 && detik == 10) {
+        modulPakan(); 
+    }
+
+    if(jam == 13 && menit == 49 && detik == 30) {
+        modulPakan(); 
+    }
+
+    if(jam == 13 && menit == 49 && detik == 50) {
+        modulPakan(); 
+    }
+    // KONDISI MODUL PAKAN
 }
+
+// MODUL PAKAN
+void modulPakan() {
+    // proses perputaran pakan secara halus
+    for (int posisi = 0; posisi <= 180; posisi++) {
+        // set posisi servo berdasarkan variabel posisi
+        servoku.write(posisi);
+        delay(10);
+    }
+
+    // proses perputaran tempat pakan kembali ke posisi awal
+    for (int posisi = 180; posisi >= 0; posisi--) {
+        servoku.write(posisi);
+        delay(10);
+    }
+}
+// MODUL PAKAN
