bd675092db8d92bfe449a44b294eb2e481cac034
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-06-15 15:47:04 +0700

    pindah ke unused

--- unused/modul_gabung/modul_gabung.ino
+++ unused/modul_gabung/modul_gabung.ino
@@ -1,170 +1,278 @@
-//KOMPONEN MODUL PAKAN
+//dht11
+#include <DHT.h>
+DHT dht(7, DHT11); //Pin, Jenis DHT
+
 //rtc
 #include "RTClib.h"
 RTC_DS3231 rtc;
 char dataHari[7][12] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
 String hari;
 int tanggal, bulan, tahun, jam, menit, detik;
+//rtc
 
 //servo
 #include <Servo.h>
 Servo servoku;
-//KOMPONEN MODUL PAKAN
+//servo
 
-//KOMPONEN MODUL MINUM
 //sensor ketinggian air
-//penampung nilai ketinggian air
 int analogPin = A1;
-int sensorVal;
-//KOMPONEN MODUL MINUM
+int ketinggianAir;
+//sensor ketinggian air
 
-//KOMPONEN MODUL MINUM
-//relay
-const int kipas1 = 10; //relay1 suhu KOMPONEN MODUL SUHU
-const int kipas2 = 11; //relay2 kelembaban KOMPONEN MODUL KELEMBABAN
-const int pompa = 12; //relay3 KOMPONEN MODUL MINUM
-const int lampu = 13; //relay4 KOMPONEN MODUL SUHU
+// relay
+const int kipas1 = 10; //relay1 suhu
+const int kipas2 = 11; //relay2 kelembaban
+const int pompa = 12; //relay3
+const int lampu = 13; //relay4
 
 //on off relay
 int relayON = LOW; //relay nyala
 int relayOFF = HIGH; //relay mati
+// relay
 
-void setup () {
-  Serial.begin(9600);
+bool statusKipasSuhu;
+bool statusKipasKelembaban;
+bool statusPompa;
+bool statusLampu;
 
-  // SETUP MODUL PAKAN
-  servoku.attach(3);
+void setup() {
+    Serial.begin(9600);
 
-  if (! rtc.begin()) {
-    Serial.println("RTC Tidak Ditemukan");
-    Serial.flush();
-    abort();
-  }
+    //  dht11
+    dht.begin();
 
-  //Atur Waktu
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  //  rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  // SETUP MODUL PAKAN
+    // servo
+    servoku.attach(6);
 
-  //  setup relay
-  pinMode(kipas1, OUTPUT);
-  pinMode(kipas2, OUTPUT);
-  pinMode(pompa, OUTPUT);
-  pinMode(lampu, OUTPUT);
-  digitalWrite(kipas1, relayOFF);
-  digitalWrite(kipas2, relayOFF);
-  digitalWrite(pompa, relayOFF);
-  digitalWrite(lampu, relayOFF);
+    // rtc ds3231
+    if (! rtc.begin()) {
+        Serial.println("RTC Tidak Ditemukan");
+        Serial.flush();
+        abort();
+    }
+
+    //Atur Waktu
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    //  rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    // rtc ds3231
+
+    //  setup relay
+    pinMode(kipas1, OUTPUT);
+    pinMode(kipas2, OUTPUT);
+    pinMode(pompa, OUTPUT);
+    pinMode(lampu, OUTPUT);
+    digitalWrite(kipas1, relayOFF);
+    digitalWrite(kipas2, relayOFF);
+    digitalWrite(pompa, relayOFF);
+    digitalWrite(lampu, relayOFF);
 }
 
-void loop () {
-  // LOOP MODUL PAKAN
-  DateTime now = rtc.now();
-  hari    = dataHari[now.dayOfTheWeek()];
-  tanggal = now.day(), DEC;
-  bulan   = now.month(), DEC;
-  tahun   = now.year(), DEC;
-  jam     = now.hour(), DEC;
-  menit   = now.minute(), DEC;
-  detik   = now.second(), DEC;
+void loop() {
+    // BACA SUHU DAN KELEMBABAN
+    float kelembaban = dht.readHumidity();
+    float suhu = dht.readTemperature();
+    // BACA SUHU DAN KELEMBABAN
 
-  Serial.println(String() + hari + ", " + tanggal + "-" + bulan + "-" + tahun);
-  Serial.println(String() + jam + ":" + menit + ":" + detik);
-  Serial.println();
+    // BACA KETINGGIAN AIR
+    ketinggianAir = analogRead(analogPin);
 
-   if(jam == 14 && menit == 28 && detik == 10) {
-    modulPakan(); 
-  }
+    // KONDISI MODUL MONITORING SUSHU DAN KELEMBABAN
+    //  suhu normal
+    if (suhu >= 29 && suhu <= 31) {
+        matikanModulPeningkatanSuhu();
+        matikanModulPenurunanSuhu();
 
-  if(jam == 14 && menit == 28 && detik == 30) {
-    modulPakan(); 
-  }
+    //  suhu di bawah standar minimum
+    } else if (suhu < 29) {
+        modulPeningkatanSuhu();
+    
+    //  suhu di atas standar maksimal
+    } else if (suhu > 31) {
+        modulPenurunanSuhu();
 
-  if(jam == 14 && menit == 28 && detik == 50) {
-    modulPakan(); 
-  }
+    //  kelembaban normal
+    } else if (kelembaban >= 50 && kelembaban <= 70) {
+        matikanModulPeningkatanKelembaban();
+        matikanModulPenurunanKelembaban();
 
-//  if(jam == 7 && menit == 0 && detik == 0) {
-//    modulPakan(); 
-//  }
-//
-//  if(jam == 12 && menit == 0 && detik == 0) {
-//    modulPakan(); 
-//  }
-//
-//  if(jam == 17 && menit == 0 && detik == 0) {
-//    modulPakan(); 
-//  }
-  // LOOP MODUL PAKAN
+    //  kelembaban di bawah standar minimum
+    } else if (kelembaban < 50) {
+        modulPeningkatanKelembaban();
+        
+    //  kelembaban di atas standar maksimal
+    } else if (kelembaban > 70) {
+        modulPenurunanKelembaban();
+    }
+    // KONDISI MODUL MONITORING SUSHU DAN KELEMBABAN
+    
+    // KONDISI MODUL MINUM
+    // kondisi coba2
+    if (ketinggianAir < 100) {
+        modulIsiBakMinum();
 
-  // LOOP MODUL MINUM
-  sensorVal = analogRead(analogPin);
+    } else if (ketinggianAir > 300) {
+        matikanModulIsiBakMinum();
+    }
+    // KONDISI MODUL MINUM
 
-  if (sensorVal <= 480) {
-    Serial.println("Ketinggian air: 0mm");
-  
-  } else if (sensorVal > 480 && sensorVal <= 530) {
-    Serial.println("Ketinggian air: 0mm - 5mm");
-  
-  } else if (sensorVal > 530 && sensorVal <= 615) {
-    Serial.println("Ketinggian air: 5mm - 10mm");
-  
-  } else if (sensorVal > 615 && sensorVal <= 660) {
-    Serial.println("Ketinggian air: 10mm - 15mm");
-  
-  } else if (sensorVal > 660 && sensorVal <= 680) {
-    Serial.println("Ketinggian air: 15mm - 20mm");
-  
-  } else if (sensorVal > 680 && sensorVal <= 690) {
-    Serial.println("Ketinggian air: 20mm - 25mm");
-  
-  } else if (sensorVal > 690 && sensorVal <= 700) {
-    Serial.println("Ketinggian air: 25mm - 30mm");
-  
-  } else if (sensorVal > 700 && sensorVal <= 705) {
-    Serial.println("Ketinggian air: 30mm - 35mm");
-  
-  } else if (sensorVal > 705) {
-    Serial.println("Ketinggian air: 35mm - 40mm");
-  }
+    // BACA WAKTU
+    DateTime now = rtc.now();
+    hari    = dataHari[now.dayOfTheWeek()];
+    tanggal = now.day(), DEC;
+    bulan   = now.month(), DEC;
+    tahun   = now.year(), DEC;
+    jam     = now.hour(), DEC;
+    menit   = now.minute(), DEC;
+    detik   = now.second(), DEC;
+    // BACA WAKTU
 
-//  if (sensorVal < 530) {
-//    modulIsiBakMinum();
-//  
-//  } else if (sensorVal > 705) {
-//    matikanModulIsiBakMinum();
-//  }
+    // KONDISI MODUL PAKAN
+    // kondisi coba2
+    if(jam == 13 && menit == 49 && detik == 10) {
+        modulPakan(); 
+    }
 
-  if (sensorVal < 100) {
-    modulIsiBakMinum();
-  
-  } else if (sensorVal > 300) {
-    matikanModulIsiBakMinum();
-  }
-  // LOOP MODUL MINUM
+    if(jam == 13 && menit == 49 && detik == 30) {
+        modulPakan(); 
+    }
+
+    if(jam == 13 && menit == 49 && detik == 50) {
+        modulPakan(); 
+    }
+    // KONDISI MODUL PAKAN
+
+    // KONDISI MODUL MODE MALAM
+    // kondisi coba2
+    if (jam == 13 && menit == 0 && detik == 0) {
+        matikanModulModeMalam();
+    
+    } else if (jam == 13 && menit == 2 && detik == 0) {
+        modulModeMalam();
+    }
+    // KONDISI MODUL MODE MALAM
+
+    // KIRIM DATA KE NODEMCU
+    modulKirimData();
+    // KIRIM DATA KE NODEMCU
 }
 
-void modulPakan() {
-  // proses perputaran pakan secara halus
-  for (int posisi = 0; posisi <= 180; posisi++) {
-    // set posisi servo berdasarkan variabel posisi
-    servoku.write(posisi);
-    delay(10);
-  }
-
-  // proses perputaran tempat pakan kembali ke posisi awal
-  for (int posisi = 180; posisi >= 0; posisi--) {
-    servoku.write(posisi);
-    delay(10);
-  }
-}
-
+// MODUL MINUM
 void modulIsiBakMinum() {
-  //relay3
-  digitalWrite(pompa, relayON);
-}
+    // mengisi bak minum dengan menyala pompa air ketika bak minum hampir kosong
+    //relay3
+    digitalWrite(pompa, relayON);
+    statusPompa = HIGH;
+    }
 
 void matikanModulIsiBakMinum() {
-  //relay3
-  digitalWrite(pompa, relayOFF);
+    // mematikan pompa air ketika bak minum sudah hampir penuh
+    //relay3
+    digitalWrite(pompa, relayOFF);
+    statusPompa = LOW;
 }
+// MODUL MINUM
+
+// MODUL MONITORING SUHU DAN KELEMBABAN
+void modulPeningkatanSuhu() {
+    //meningkatkan suhu kandang ketika suhu di bawah normal dengan menyalakan lampu
+    //relay4
+    digitalWrite(lampu, relayON);
+    statusLampu = HIGH;
+}
+
+void matikanModulPeningkatanSuhu() {
+    //mematikan modul peningkatan suhu ketika suhu normal tercapai dengan mematikan lampu
+    //relay4
+    digitalWrite(lampu, relayOFF);
+    statusLampu = LOW;
+}
+
+void modulPenurunanSuhu() {
+    //menurunkan suhu kandang ketika suhu di atas normal dengan menyalakan kipas1
+    //relay1
+    digitalWrite(kipas1, relayON);
+    statusKipasSuhu = HIGH;
+}
+
+void matikanModulPenurunanSuhu() {
+    //mematikan modul penurunan suhu ketika suhu normal tercapai dengan mematikan kipas1
+    //relay1
+    digitalWrite(kipas1, relayOFF);
+    statusKipasSuhu = LOW;
+}
+
+void modulPeningkatanKelembaban() {
+    //meningkatkan kelembaban kandang ketika kelembaban di bawah normal dengan menyalakan kipas
+    //relay2
+    digitalWrite(kipas2, relayON);
+    statusKipasKelembaban = HIGH;
+}
+
+void matikanModulPeningkatanKelembaban() {
+    //mematikan modul peningkatan kelembaban ketika kelembaban normal tercapai dengan mematikan kipas
+    //relay2
+    digitalWrite(kipas2, relayOFF);
+    statusKipasKelembaban = LOW;
+}
+
+void modulPenurunanKelembaban() {
+    //menurunkan kelembaban kandang ketika kelembaban di atas normal dengan menyalakan lampu
+    //relay4
+    digitalWrite(lampu, relayON);
+    statusLampu = HIGH;
+}
+
+void matikanModulPenurunanKelembaban() {
+    //mematikan modul penurunan kelembaban ketika kelembaban normal tercapai dengan mematikan lampu
+    //relay4
+    digitalWrite(lampu, relayOFF);
+    statusLampu = LOW;
+}
+// MODUL MONITORING SUHU DAN KELEMBABAN
+
+// MODUL PAKAN
+void modulPakan() {
+    // proses perputaran pakan secara halus
+    for (int posisi = 0; posisi <= 180; posisi++) {
+        // set posisi servo berdasarkan variabel posisi
+        servoku.write(posisi);
+        delay(10);
+    }
+
+    // proses perputaran tempat pakan kembali ke posisi awal
+    for (int posisi = 180; posisi >= 0; posisi--) {
+        servoku.write(posisi);
+        delay(10);
+    }
+}
+// MODUL PAKAN
+
+// MODUL MODE MALAM
+void modulModeMalam() {
+    // menyalakan lampu kandang ketika waktu sudah memasuki malam hari
+    //relay4
+    digitalWrite(lampu, relayON);
+    statusLampu = HIGH;
+}
+
+void matikanModulModeMalam() {
+    // mematikan lampu kandang ketika waktu sudah memasuki pagi hari
+    //relay4
+    digitalWrite(lampu, relayOFF);
+    statusLampu = LOW;
+}
+// MODUL MODE MALAM
+
+// MODUL KIRIM DATA
+void modulKirimData() {
+    // BACA SUHU DAN KELEMBABAN
+    float kelembaban = dht.readHumidity();
+    float suhu = dht.readTemperature();
+    // BACA SUHU DAN KELEMBABAN
+    
+    String dataKirim = "#" + String(ketinggianAir) + "#" + String(suhu) + "#" + String(kelembaban) + "#" + String(String() + jam + ":" + menit + ":" + detik) + "#" + String(statusKipasSuhu) + "#" + String(statusKipasKelembaban) + "#" + String(statusPompa) + "#" + String(statusLampu);
+    Serial.println(dataKirim);
+    delay(1000);
+}
+// MODUL KIRIM DATA
